5a20c81a321d4b441aed9c9e6095b2319bf86074
hungthe07 <nguyenhungeyyy07@gmail.com>
2025-11-17 22:07:47 +0700

    Add files via upload

---
+++ [Cơ bản] 1. Khởi tạo Cơ sở dữ liệu và Bảng.sql
@@ -0,0 +1,14 @@
+create database QuanLyThuVien;
+use QuanLyThuVien;
+create table TacGia (
+MaTacGia int primary key auto_increment,
+TenTacGia varchar(100) not null,
+QuocTich varchar(50)
+);
+create table DocGia (
+MaDocGia int primary key auto_increment,
+TenDocGia varchar(100) not null,
+DiaChi varchar(255),
+SoDienThoai varchar(15) unique
+
+);
