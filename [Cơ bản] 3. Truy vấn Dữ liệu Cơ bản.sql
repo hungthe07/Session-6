eff3b6e06480f99786e2434d7f369c17a7be7bb1
hungthe07 <nguyenhungeyyy07@gmail.com>
2025-11-17 22:45:29 +0700

    Add files via upload

---
+++ [Cơ bản] 3. Truy vấn Dữ liệu Cơ bản.sql
@@ -0,0 +1,36 @@
+create database QuanLyThuVien;
+use QuanLyThuVien;
+create table TacGia (
+MaTacGia int primary key auto_increment,
+TenTacGia varchar(100) not null,
+QuocTich varchar(50)
+);
+create table DocGia (
+MaDocGia int primary key auto_increment,
+TenDocGia varchar(100) not null,
+DiaChi varchar(255),
+SoDienThoai varchar(15) unique
+
+);
+insert into TacGia (TenTacGia, QuocTich)
+values
+('Nguyễn Nhật Ánh', 'Việt Nam'),
+('J.K. Rowling', 'Anh'),
+('Haruki Murakami', 'Nhật Bản'),
+('Dale Carnegie', 'Mỹ');
+
+insert into DocGia (TenDocGia, DiaChi,SoDienThoai)
+values 
+('Nguyễn Văn A', '123 Đường ABC, Hà Nội', '0901234567'),
+('Trần Thị B', '456 Đường XYZ, TP.HCM', '0912345678'),
+('Lê Văn C', '789 Đường LMN, Đà Nẵng', '0923456789'),
+('Phạm Thị D', '101 Đường QRS, Hà Nội', '0934567890'),
+('Hoàng Văn E', '202 Đường UVW, Cần Thơ', '0945678901');
+
+select * from TacGia 
+where QuocTich = 'Việt Nam';
+
+select * from DocGia
+where DiaChi like '%Hà Nội';
+
+select TenDocGia, SoDienThoai from DocGia;
